c838027c6c7ac46ebb275121450cf59b5fe4ec1c
Abdulhameed735 <abdulhameedmoonstapha735@gmail.com>
2022-11-02 11:48:15 +0100

    initial commit - setting up

---
+++ assets/index.js
@@ -0,0 +1,52 @@
+//** */ SVG IMAGES/ICONS
+
+import logo from './svgs/logo.svg';
+import right from './svgs/right.svg';
+import left from './svgs/left.svg';
+import down from './svgs/down.svg';
+import map from './svgs/map.svg';
+import facebook from './svgs/facebook.svg';
+import instagram from './svgs/instagram.svg';
+import pinterest from './svgs/pinterest.svg';
+import spotify from './svgs/spotify.svg';
+import twitter from './svgs/twitter.svg';
+import youtube from './svgs/youtube.svg';
+
+//** */ HOMEPAGE IMAGES
+import image1 from './images/image-1.webp';
+import image2 from './images/image-2.webp';
+import image3 from './images/image-3.jpg';
+import image4 from './images/image-4.jpg';
+import image5 from './images/image-5.jpg';
+
+//** */ CUSTOM FONTS
+import thin from './fonts/Thin.ttf';
+import light from './fonts/Light.ttf';
+import regular from './fonts/Regular.ttf';
+import semiBold from './fonts/SemiBold.ttf';
+import bold from './fonts/Bold.ttf';
+
+//** */ Export SVG images and icons
+export {
+    logo, 
+    right, 
+    left, 
+    down, 
+    map, 
+    facebook, 
+    instagram, 
+    pinterest, 
+    spotify, 
+    twitter, 
+    youtube
+}
+
+//** */ Export Homepage images
+export {
+    image1, image2, image3, image4, image5
+}
+
+//** */ Export Custom Fonts
+export {
+    thin, light, regular, bold, semiBold
+}
